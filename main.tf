92afd0bd80b0e8ba92ad1fde209ef8aaae69a19e
suraj0233 <160613987+suraj0233@users.noreply.github.com>
2025-01-31 14:03:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 
+provider "aws" {
+  region = "ap-south-1"  # Adjust your region
+}
 
-resource "aws_instance" "myserver" {
-    ami = "ami-0dee22c13ea7a9a67"
-    instance_type = "t2.nano"
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00bb6a80f01f03502"  
+  instance_type = "t2.micro"
 
-    tags = {
-      name = "sample_server"
-    }
-
-  
+  tags = {
+    Name = "Suraj "
+  }
 }
